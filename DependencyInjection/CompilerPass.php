79d927e7d8d95183e7d0399af2b13b6f6bef5961
Martin Poirier-Theoret <mpoiriert@gmail.com>
2018-06-25 16:49:18 -0400

    clean u yml file and fake a soft dependency on fos/rest-bundle

--- DependencyInjection/CompilerPass.php
+++ DependencyInjection/CompilerPass.php
@@ -2,6 +2,7 @@
 
 namespace Draw\DrawBundle\DependencyInjection;
 
+use Draw\DrawBundle\Request\RequestBodyParamConverter;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -18,12 +19,20 @@ class CompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $requestBodyConverter = $container->getDefinition("fos_rest.converter.request_body");
+        $requestBodyConverter = null;
+        if($container->hasDefinition("fos_rest.converter.request_body")) {
+            $requestBodyConverter = $container->getDefinition("fos_rest.converter.request_body");
+        } elseif($container->hasDefinition(\FOS\RestBundle\Request\RequestBodyParamConverter::class)) {
+            $requestBodyConverter = $container->getDefinition(\FOS\RestBundle\Request\RequestBodyParamConverter::class);
+        }
 
-        $requestBodyConverter->addMethodCall(
+        if(!is_null($requestBodyConverter)) {
+            $requestBodyConverter->setClass(RequestBodyParamConverter::class);
+            $requestBodyConverter->addMethodCall(
                 "setGroupHierarchy",
                 [new Reference("draw.serializer.group_hierarchy")]
             );
+        }
 
         if($container->hasDefinition('jms_serializer.doctrine_object_constructor')) {
             $container->getDefinition("jms_serializer.doctrine_object_constructor")
